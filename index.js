30b944c1195c751a2bb98794494caf20e85ec585
JBWERE-coder <lutherwere@gmail.com>
2023-06-20 14:13:32 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-function addingEventListener() {
+function addingEventListener(clickAlert) {
+  alert('I was clicked!');
+  const input = document.getElementById('input');
+  input.addEventListener('click', clickAlert);
 }
+
+
+
+
+//function clickAlert() {}
+
